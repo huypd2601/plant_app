cc5ed8829047f065458e9e8f14fba82841537d4d
HUY PHAM DUC <huyphamduc26.01@gmail.com>
2023-05-31 22:24:24 +0700

    31/5/2023 - Huy

--- app/src/main/java/com/example/app2/SpeciesActivity.kt
+++ app/src/main/java/com/example/app2/SpeciesActivity.kt
@@ -1,65 +1,99 @@
 package com.example.app2
 
-import `in`.myinnos.alphabetsindexfastscrollrecycler.IndexFastScrollRecyclerView
+//import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Observer
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.example.app2.databinding.ActivitySpeciesBinding
 import java.util.*
+import `in`.myinnos.alphabetsindexfastscrollrecycler.IndexFastScrollRecyclerView
 
-private const val TAG = "SpeciesActivity"
 
-class SpeciesActivity {
-//    : AppCompatActivity(), SpeciesAdapter.OnItemClickListener{
-//    var mRecyclerView: IndexFastScrollRecyclerView? = null
-//
-//    private val speciesNameList : ArrayList<String> = arrayListOf()
-//    private val db = Firebase.firestore
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_species)
-//        mRecyclerView = findViewById(R.id.fast_scroller_recycler)
-//
-//        db.collection("species")
-//            .get()
-//            .addOnSuccessListener { result ->
-//                for (document in result) {
-//                    Log.d(TAG, "${document.id} => ${document.data}")
-//                    val species : String = document.id;
-//                    //println(species)
-//                    speciesNameList.add(species)
-//                }
-//                initialiseUI()
-//            }
-//            .addOnFailureListener { exception ->
-//                Log.d(TAG, "Error getting documents: ", exception)
-//            }
-//    }
-//
-//    override fun OnItemClick(position: Int) {
-//
-//    }
-//
-//    private fun initialiseUI() {
-//        mRecyclerView?.apply {
-//            layoutManager = LinearLayoutManager(this@SpeciesActivity)
-//            adapter = SpeciesAdapter(speciesNameList, listener : this)
-//            setIndexTextSize(12)
-//            setIndexBarCornerRadius(0)
-//            setIndexBarTransparentValue(0.4.toFloat())
-//            setPreviewPadding(0)
-//            setPreviewTextSize(60)
-//            setPreviewTransparentValue(0.6f)
-//            setIndexBarVisibility(true)
-//            setIndexBarStrokeVisibility(true)
-//            setIndexBarStrokeWidth(1)
-//            setIndexBarHighLightTextVisibility(true)
-//        }
-//        Objects.requireNonNull<RecyclerView.LayoutManager>(mRecyclerView?.layoutManager)
-//            .scrollToPosition(0)
-//    }
+class SpeciesActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySpeciesBinding
+    lateinit var adapter: SpeciesAdapter
+    lateinit var viewModel: SpeciesVM
+    lateinit var searchView: SearchView
+    lateinit var searchList: List<Species>
+    lateinit var dataList: List<Species>
+    var mRecyclerView: IndexFastScrollRecyclerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding   = DataBindingUtil.setContentView(this,R.layout.activity_species)
+        viewModel = ViewModelProvider(this)[SpeciesVM::class.java]
+        searchView = findViewById(R.id.search)
+        dataList = arrayListOf<Species>()
+        searchList = arrayListOf<Species>()
+        searchView.clearFocus()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val searchText = newText!!.toLowerCase(Locale.getDefault())
+                viewModel.filter(searchText)
+                registerDataEvent()
+                return false
+            }
+        })
+
+        setUpRecyclerView()
+        initialiseUI()
+        registerDataEvent()
+
+    }
+
+    private fun setUpRecyclerView() {
+        binding.speciesListRecycleView.layoutManager = LinearLayoutManager(this);
+        mRecyclerView = findViewById(R.id.species_list_recycle_view)
+        adapter = SpeciesAdapter(OnSpeciesClickListener)
+        binding.speciesListRecycleView.adapter = adapter
+        viewModel.loadData()
+    }
+
+    private val OnSpeciesClickListener  = object : OnSpeciesItemListener {
+        override fun onClickItem(item: Species) {
+            viewModel.handleItemWhenClicked(this@SpeciesActivity,item)
+        }
+    }
+
+    private fun registerDataEvent() {
+        viewModel.listOfSpecies.observe(this, Observer{ data ->
+            run{
+                adapter.submitList(data)
+            }
+        })
+    }
+
+    private fun initialiseUI() {
+        mRecyclerView?.apply {
+            layoutManager = LinearLayoutManager(this@SpeciesActivity)
+            setIndexTextSize(12)
+            setIndexBarCornerRadius(0)
+            setIndexBarTransparentValue(0.4.toFloat())
+            setPreviewPadding(0)
+            setPreviewTextSize(60)
+            setPreviewTransparentValue(0.6f)
+            setIndexBarVisibility(true)
+            setIndexBarStrokeVisibility(true)
+            setIndexBarStrokeWidth(1)
+            setIndexBarHighLightTextVisibility(true)
+        }
+        Objects.requireNonNull<RecyclerView.LayoutManager>(mRecyclerView?.layoutManager)
+            .scrollToPosition(0)
+    }
+
 }
+
+
+
+
+
---
+++ app/src/main/java/com/example/app2/SpeciesViewHolder.kt
@@ -0,0 +1,28 @@
+package com.example.app2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class SpeciesViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView) {
+    companion object {
+        fun from(parent: ViewGroup): SpeciesViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view =
+                layoutInflater.inflate(
+                    R.layout.species_items,
+                    parent,
+                    false
+                )
+            return SpeciesViewHolder(view)
+        }
+    }
+
+    fun bindData(species: Species, callback: OnSpeciesItemListener) {
+        val speciesName = itemView.findViewById<TextView>(R.id.species_name);
+        speciesName.text = species.name
+        itemView.setOnClickListener { callback.onClickItem(species) }
+    }
+}
