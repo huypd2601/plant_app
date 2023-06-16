58c2e7b12c93b5efa1e68b4af6cbd843c790f4c2
HUY PHAM DUC <huyphamduc26.01@gmail.com>
2023-06-16 09:31:35 +0700

    16/6/2023_fragment - Huy

--- app/src/main/java/com/example/app2/profileFragment.kt
+++ app/src/main/java/com/example/app2/profileFragment.kt
@@ -1,22 +1,10 @@
 package com.example.app2
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import java.io.File
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,67 +20,21 @@ class profileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private val PICK_IMAGE_REQUEST = 71
-    private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var imageView: ImageView
-    private lateinit var firebaseStorage: FirebaseStorage
-    private lateinit var storageReference: StorageReference
-    private lateinit var firestore: FirebaseFirestore
-    private var filePath: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        firebaseAuth = FirebaseAuth.getInstance()
-        firebaseStorage = FirebaseStorage.getInstance()
-        storageReference = FirebaseStorage.getInstance().reference
-        firestore = FirebaseFirestore.getInstance()
     }
 
-    @SuppressLint("MissingInflatedId", "CutPasteId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
-        val userName = view.findViewById(R.id.tv_nameUser) as TextView
-        val email = view.findViewById(R.id.tv_emailUser) as TextView
-        firestore.collection("profile").document("profile")
-            .collection("profile").document(firebaseAuth.uid.toString())
-            .get().addOnSuccessListener {
-                val data : MutableMap<String,Any> = it.data!!
-                userName.text = data["UserName"].toString()
-                email.text = data["Email"].toString()
-            }
-        imageView = view.findViewById(R.id.iv_avatarProfile)
-        val ref = storageReference.child(firebaseAuth.uid.toString())
-        ref.getBytes(10 * 1024 * 1024).addOnSuccessListener {
-            val bitmap = BitmapFactory.decodeByteArray(it, 0, it.size)
-            imageView.setImageBitmap(bitmap)
-        }
-        imageView.setOnClickListener {
-            launchGallery()
-        }
-        return view
-    }
-
-    private fun launchGallery() {
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            filePath = data!!.data
-            imageView.setImageURI(filePath)
-            val ref = storageReference.child(firebaseAuth.uid.toString())
-            ref.putFile(filePath!!)
-        }
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 
